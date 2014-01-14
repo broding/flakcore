828e8282ad6507ae461ddc8dbba86b484fbe5a09
Bas Roding <bas.roding@gmail.com>
2014-01-14 16:21:56 +0100

    ConvexShape can now produce axis and a clone

--- src/Physics/ConvexShape.cs
+++ src/Physics/ConvexShape.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace FlakCore.Physics
 {
-	public class ConvexShape
+	public class ConvexShape : ICloneable
 	{
 		public const int MAX_POINTS = 15;
 
@@ -20,6 +20,27 @@ namespace FlakCore.Physics
 			Points [index] = position;
 		}
 
+		public List<Vector2> axis
+		{
+			get 
+			{
+				List<Vector2> axis = new List<Vector2>(Points.Count);
+
+				for(int i = 0; i < Points.Count; i++)
+				{
+					Vector2 p1 = Points [i];
+					Vector2 p2 = Points[i + 1 == Points.Count ? 0 : i + 1];
+					Vector2 edge = p2 - p1;
+					Vector2 normalizedEdge = new Vector2 (edge.X, edge.Y);
+					normalizedEdge.Normalize ();
+					Vector2 normal = Vector2Utils.Perpendicular (normalizedEdge);
+					axis.Add (normal);
+				}
+
+				return axis;
+			}
+		}
+
 		public static ConvexShape operator *(ConvexShape shape, Matrix matrix)
 		{
 			for (int i = 0; i < shape.Points.Count; i++) 
@@ -29,6 +50,19 @@ namespace FlakCore.Physics
 
 			return shape;
 		}
+
+		public object Clone()
+		{
+			ConvexShape shape = new ConvexShape ();
+
+			foreach (Vector2 point in Points) 
+			{
+				shape.Points.Add (new Vector2 (point.X, point.Y));
+			}
+
+			return shape;
+		}
+
 	}
 }
 
